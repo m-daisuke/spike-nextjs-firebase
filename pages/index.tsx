cfe69bda811bf782927a011b7664cfb85efe8765
yossy11 <ayoshimuradev@gmail.com>
2021-09-10 01:36:41 +0900

    feat: 画面遷移の追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,42 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { FormEvent, useState } from 'react'
 import { Container, Row, Button, Form, Col } from 'react-bootstrap'
+import { UserInputs } from '../lib/types'
 
 const Home: NextPage = () => {
   const [validated, setValidated] = useState(false)
+  const [state, setState] = useState<UserInputs>({
+    name: '',
+    number1: '',
+    number2: '',
+  })
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    const form = event.currentTarget
-    if (!form.checkValidity()) {
-      event.preventDefault()
-      event.stopPropagation()
-    }
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [event.target.id]: event.target.value })
+  }
+
+  const router = useRouter()
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     setValidated(true)
+    event.preventDefault()
+    event.stopPropagation()
+    const form = event.currentTarget
+    if (form.checkValidity()) {
+      const response = await fetch('api/calc', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(state),
+      })
+      // console.log(await response.json())
+      router.push('/result')
+    }
   }
 
   return (
@@ -22,13 +46,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
-        <Form.Group controlId="yourname" className="mb-3">
+        <Form.Group controlId="name" className="mb-3">
           <Row className="justify-content-center">
             <Col md="2">
-              <Form.Label>名前</Form.Label>
+              <Form.Label className="m-0 align-middle">名前</Form.Label>
             </Col>
             <Col md="6">
-              <Form.Control type="text" placeholder="Enter name" required />
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                required
+                value={state.name}
+                onChange={handleChange}
+              />
               <Form.Control.Feedback type="invalid">
                 名前を入力してください
               </Form.Control.Feedback>
@@ -47,6 +77,8 @@ const Home: NextPage = () => {
                     type="number"
                     placeholder="Enter number1"
                     required
+                    value={state.number1}
+                    onChange={handleChange}
                   />
                   <Form.Control.Feedback type="invalid">
                     数字を入力してください
@@ -66,6 +98,8 @@ const Home: NextPage = () => {
                     type="number"
                     placeholder="Enter number2"
                     required
+                    value={state.number2}
+                    onChange={handleChange}
                   />
                   <Form.Control.Feedback type="invalid">
                     数字を入力してください
