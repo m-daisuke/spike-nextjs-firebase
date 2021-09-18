3530b5e0d78b9ca3537ce560ae1b717fdfa8d831
yossy11 <ayoshimuradev@gmail.com>
2021-09-18 20:01:25 +0900

    feat: hosting,functions共通の型定義や処理を記述するworkspaceを作成

---
+++ common/src/types.ts
@@ -0,0 +1,9 @@
+export type Data = {
+  answer: number;
+};
+
+export type UserInputs = {
+  name: string;
+  number1: number | ""; // 初期値としてnull | undefinedを入れてしまうとuncontrolled input warningが出てしまうため空文字('')を入れる
+  number2: number | "";
+};
