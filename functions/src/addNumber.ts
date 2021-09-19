393db03a3c98b38bdfe147e561e91ea79cf50913
yossy11 <ayoshimuradev@gmail.com>
2021-09-19 14:21:11 +0900

    feat: lintの設定を見直し

--- functions/src/addNumber.ts
+++ functions/src/addNumber.ts
@@ -1,25 +1,26 @@
-import * as functions from 'firebase-functions'
-import * as corsLib from 'cors'
+import * as functions from "firebase-functions";
+import * as corsLib from "cors";
 
 export const addNumber = functions
-  .region('asia-northeast1')
+  .region("asia-northeast1")
   .https.onRequest((req, res) => {
-    functions.logger.info(`Request received!`)
-    const cors = corsLib()
+    functions.logger.info(`Request received!`);
+    const cors = corsLib();
     cors(req, res, () => {
-      if ('number1' in req.body && 'number2' in req.body) {
-        const userInputs = req.body
+      if ("number1" in req.body && "number2" in req.body) {
+        const userInputs = req.body;
         try {
-          const answer = Number(userInputs.number1) + Number(userInputs.number2)
-          res.status(200).json({ answer: answer })
-          functions.logger.info(`The answer is ${answer}`)
+          const answer =
+            Number(userInputs.number1) + Number(userInputs.number2);
+          res.status(200).json({ answer: answer });
+          functions.logger.info(`The answer is ${answer}`);
         } catch (e) {
-          res.status(400).json('Invalid Request!')
-          functions.logger.info(`Invalid Request!`)
+          res.status(400).json("Invalid Request!");
+          functions.logger.info(`Invalid Request!`);
         }
       } else {
-        res.status(400).json('Invalid Request!!')
-        functions.logger.info(`Invalid Request!!`)
+        res.status(400).json("Invalid Request!!");
+        functions.logger.info(`Invalid Request!!`);
       }
-    })
-  })
+    });
+  });
